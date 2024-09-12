170940ea88e3328484f5f5c3f1fc068b8d13321c
DustVoyager <ohjamdol@gmail.com>
2024-09-12 13:37:58 +0900

    header,footer, page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,18 +2,39 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <h1>
+    <header className="flex my-10 md:mb-10 items-center font-mono">
+      <h1 className="text-xl whitespace-nowrap font-bold">
         <Link href="/">Dust Voyager</Link>
       </h1>
-      <ul>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="resume">Resume</Link>
-        </li>
-      </ul>
+
+      <nav className="text-xs grow flex justify-end items-center">
+        <ul className="flex gap-2 md:gap-3">
+          <li className="inline-flex">
+            <Link
+              href="/about"
+              className="hover:text-white hover:bg-blue-400 active:bg-blue-500 rounded-sm p-2 transition-[background-color]"
+            >
+              About
+            </Link>
+          </li>
+          <li className="inline-flex">
+            <Link
+              href="/post"
+              className="hover:text-white hover:bg-blue-400 active:bg-blue-500 rounded-sm p-2 transition-[background-color]"
+            >
+              Post
+            </Link>
+          </li>
+          <li className="inline-flex">
+            <Link
+              href="/resume"
+              className="hover:text-white hover:bg-blue-400 active:bg-blue-500 rounded-sm p-2 transition-[background-color]"
+            >
+              Resume
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
